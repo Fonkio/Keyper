a880ac40514305bd84ec85f80d9b081ed3bbe214
Fonkio <maxime.fabre10@gmail.com>
2019-03-05 11:38:46 +0100

    Ajout du menu

--- app/src/main/java/com/example/keyper/MainActivity.java
+++ app/src/main/java/com/example/keyper/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.keyper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
     }
 }
